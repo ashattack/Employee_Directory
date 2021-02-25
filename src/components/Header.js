d576b043ef1f42472c67bb474b9aa6a6cc9c96e9
Ashlyn Hicks <ash.giselle96@gmail.com>
2021-02-25 17:44:38 -0500

    allows you to search through employees

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles/Header.css'
+function Header() {
+    return (
+        <div id="header">
+            <h1>Employee Directory</h1>
+            <p>Click on carrots to filter by heading or use the search box to narrow your results</p>
+        </div>
+    )
+}
+export default Header;
